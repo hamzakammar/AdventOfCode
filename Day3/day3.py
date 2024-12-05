c62d294cbc5bdc09ac595234caa9550118ffe061
Hamza <hamza.k.ammar@gmail.com>
2024-12-05 09:48:34 -0500

    Day 3 Part 1

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,4 +1,15 @@
+import re
+
 f = open('Day3/day3.txt', 'r')
 code = f.read()
 
-fulllist = code.split('\n')
+# Updated regex to match only valid mul instructions
+# \d{1,3} matches 1-3 digits
+pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+matches = re.findall(pattern, code)
+
+total = 0
+for x, y in matches:
+    total += int(x) * int(y)
+
+print("Total:", total)
--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 - [x] Part 2
 
 **DAY 3**
-- [ ] Part 1
+- [x] Part 1
 - [ ] Part 2
 
 **DAY 4**
