52c1e82418a5f9c1f1ef436385c609e50a0ff305
Hamza <hamza.k.ammar@gmail.com>
2025-12-06 23:59:25 -0500

    Right on time

--- AdventOfCode_2025/Day6/day6.py
+++ AdventOfCode_2025/Day6/day6.py
@@ -1,8 +1,11 @@
+import re 
+import math
+
 with open("input.txt") as file:
     lines = [line.rstrip('\n') for line in file.readlines()]
     n = len(lines[0])
 
-    sum = 0
+    total = 0
     i = 0
     while i < n:
         if all(lines[row][i] == ' ' for row in range(len(lines))):
@@ -26,6 +29,36 @@ with open("input.txt") as file:
             result = 1
             for num in numbers:
                 result *= num
-        sum += result
+        total += result
         i = problem_end
-    print(sum)
+    print(total)
+
+#Part 2:
+with open("input.txt") as file:
+    data_list = file.read().splitlines()
+    col_len_list = [len(col) for col in re.split(r"\s(?=[+\*])", data_list[-1])]
+    col_list = []
+    for row in data_list:
+        cols = []
+        idx = 0
+        for col_len in col_len_list:
+            idx_next = idx + col_len
+            cols.append(row[idx:idx_next])
+            idx = idx_next + 1
+        col_list.append(cols)
+    col_list = list(zip(*col_list))
+    parsed_data = col_list
+    total_value = 0
+
+    for idx, col in enumerate(parsed_data):
+        rev_col = list(zip(*[list(num[::-1]) for num in col[:-1]]))
+        join_numbers = [int("".join(num).strip()) for num in rev_col]
+        answer = 0
+        match col[-1].strip():
+            case "+":
+                answer = sum(join_numbers)
+            case "*":
+                answer = math.prod(join_numbers)
+        print("Expression {} equals {}.".format(idx, answer))
+        total_value += answer
+    print(total_value)
